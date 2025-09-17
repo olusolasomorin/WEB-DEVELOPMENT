d8d7608573e4e27b29d47c73a36491120e7312ae
olusola <somorinolusola1@gmail.com>
2025-09-18 00:01:31 +0100

    initial

---
+++ JavaScript/JavaScript task/task.js
@@ -0,0 +1,34 @@
+const about = document.getElementById("about")
+const contact = document.getElementById("contact")
+const skill = document.getElementById("skill")
+
+const about_me = document.getElementById("about-me")
+const contact_me = document.getElementById("contact-me")
+const my_skill = document.getElementById("my-skill")
+
+about.onclick = () => {
+    alert("About me section clicked")
+    about_me.style.display="block"
+    contact_me.style.display="none"
+    my_skill.style.display="none"
+    about.style.backgroundColor="blue"
+    about.style.color="white"
+}
+
+contact.onclick = () => {
+    alert("About me section clicked")
+    about_me.style.display="none"
+    contact_me.style.display="block"
+    my_skill.style.display="none"
+    contact.style.backgroundColor="blue"
+    contact.style.color="white"
+}
+
+skill.onclick = () => {
+    alert("About me section clicked")
+    about_me.style.display="none"
+    contact_me.style.display="none"
+    my_skill.style.display="block"
+    skill.style.backgroundColor="blue"
+    skill.style.color="white"
+}
